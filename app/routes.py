a66a56340c1afbd924cc3ec8b74a62d1c357bb37
Michael <mnath10@stuy.edu>
2019-04-22 10:24:35 -0400

    Modified w Templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
+from flask import render_template
 from app import app 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World! My name is Michael"
-@app.route("/main")
-def main():
-    return "Boo yeah!"
+    user = {"username":"Michael"}
+    return render_template('index.html', title="Home", user=user)
+    
+
